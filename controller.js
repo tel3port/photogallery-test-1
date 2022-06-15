f271c3db49b00fd20b700e9c2efe075d8f5e6fed
tel3port <carn3lian@gmail.com>
2022-06-15 19:45:47 +0300

    search and initial image load functionalities finished

--- controller.js
+++ controller.js
@@ -1,4 +1,114 @@
 "use strict"
-// this.API_KEY = "563492ad6f917000010000011bf92e0340834b29924f9cbaf7883c1d";
+API_KEY = "563492ad6f917000010000011bf92e0340834b29924f9cbaf7883c1d";
+//`https://api.pexels.com/v1/search?query=${query}&page=${page_num}
+//`https://api.pexels.com/v1/curated?page=${page_num}`
+
 import 'regenerator-runtime'
 import 'core-js/stable'
+
+let parentEl = document.querySelector(".gallery")
+let searchFormEl = document.querySelector(".search-form")
+let searchEl = document.querySelector(".search-input")
+
+searchFormEl.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let searchText = searchEl.value.trim()
+
+    if (!searchText) return;
+
+    searchImages(searchText, 1)
+
+
+})
+
+async function searchImages(query, page_num) {
+
+    try {
+
+        let rawData = await fetch(`https://api.pexels.com/v1/search?query=${query}&page=${page_num}`,
+            {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    Authorization: API_KEY
+
+                }
+
+            })
+
+        let imageData = await (rawData.json())
+
+        displayImages(imageData);
+    } catch (error) {
+        console.log(error);
+
+    }
+
+
+}
+
+async function getCuratedPhotos(page_num) {
+
+    try {
+
+        let rawData = await fetch(`https://api.pexels.com/v1/curated?page=${page_num}`,
+            {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    Authorization: API_KEY
+
+                }
+
+            })
+
+        let imageData = await (rawData.json())
+
+        displayImages(imageData);
+    } catch (error) {
+        console.log(error);
+
+    }
+
+}
+
+function displayImages(response) {
+    clearGallery()
+
+    let html = ""
+
+    response.photos.forEach(photo => {
+        html += `
+        <div class="item">
+            <a href="${photo.url}">
+                <img src="${photo.src.original}" alt="${photo.alt}">
+                <h3>${photo.photographer}</h3>
+            </a>
+        </div>
+        `
+    })
+
+    parentEl.insertAdjacentHTML("afterbegin", html)
+
+}
+
+function clearGallery() {
+    parentEl.innerHTML = ""
+
+}
+// radom number between two nums inclusive
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+window.addEventListener("load", init)
+
+function init() {
+    let randPageNum = getRandomInt(1, 40)
+    getCuratedPhotos(randPageNum)
+
+}
+
+init()
--- dist/index.561fface.js
+++ dist/index.561fface.js
@@ -505,9 +505,80 @@ function hmrAcceptRun(bundle, id) {
 },{}],"a2PJv":[function(require,module,exports) {
 var _esArrayIncludesJs = require("core-js/modules/es.array.includes.js");
 var _webImmediateJs = require("core-js/modules/web.immediate.js");
-// this.API_KEY = "563492ad6f917000010000011bf92e0340834b29924f9cbaf7883c1d";
+//`https://api.pexels.com/v1/search?query=${query}&page=${page_num}
+//`https://api.pexels.com/v1/curated?page=${page_num}`
 var _regeneratorRuntime = require("regenerator-runtime");
 "use strict";
+API_KEY = "563492ad6f917000010000011bf92e0340834b29924f9cbaf7883c1d";
+let parentEl = document.querySelector(".gallery");
+let searchFormEl = document.querySelector(".search-form");
+let searchEl = document.querySelector(".search-input");
+searchFormEl.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let searchText = searchEl.value.trim();
+    if (!searchText) return;
+    searchImages(searchText, 1);
+});
+async function searchImages(query, page_num) {
+    try {
+        let rawData = await fetch(`https://api.pexels.com/v1/search?query=${query}&page=${page_num}`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Authorization: API_KEY
+            }
+        });
+        let imageData = await rawData.json();
+        displayImages(imageData);
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function getCuratedPhotos(page_num) {
+    try {
+        let rawData = await fetch(`https://api.pexels.com/v1/curated?page=${page_num}`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Authorization: API_KEY
+            }
+        });
+        let imageData = await rawData.json();
+        displayImages(imageData);
+    } catch (error) {
+        console.log(error);
+    }
+}
+function displayImages(response) {
+    clearGallery();
+    let html = "";
+    response.photos.forEach((photo)=>{
+        html += `
+        <div class="item">
+            <a href="${photo.url}">
+                <img src="${photo.src.original}" alt="${photo.alt}">
+                <h3>${photo.photographer}</h3>
+            </a>
+        </div>
+        `;
+    });
+    parentEl.insertAdjacentHTML("afterbegin", html);
+}
+function clearGallery() {
+    parentEl.innerHTML = "";
+}
+// radom number between two nums inclusive
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+window.addEventListener("load", init);
+function init() {
+    let randPageNum = getRandomInt(1, 40);
+    getCuratedPhotos(randPageNum);
+}
+init();
 
 },{"core-js/modules/es.array.includes.js":"dkJzX","core-js/modules/web.immediate.js":"49tUX","regenerator-runtime":"dXNgZ"}],"dkJzX":[function(require,module,exports) {
 "use strict";
